7aebd01f22313f4ff7fd9f20597d30b1b56baeec
Morten Sørensen <morten.sorensen.04@gmail.com>
2025-03-13 18:01:01 +0100

    working ntp time syncronization

--- device_src/main/hello_world_main.c
+++ device_src/main/hello_world_main.c
@@ -7,6 +7,7 @@
 #include <stdio.h>
 #include <inttypes.h>
 #include <time.h>
+#include <sys/time.h>
 #include "sdkconfig.h"
 
 #include "freertos/FreeRTOS.h"
@@ -22,13 +23,25 @@
 #include "esp_mac.h"
 #include "esp_event.h"
 #include "esp_log.h"
+#include "esp_ping.h"
+
+#include "ping/ping.h"
+#include "ping/ping_sock.h"
+
+#include "esp_sntp.h"
+// #include "lwip/apps/sntp.h"
+// #include "lwip/priv/tcpip_priv.h"
+#include "esp_macros.h"
+#include "esp_attr.h"
+#include "esp_sleep.h"
+#include "esp_netif_sntp.h"
 
 #include "lwip/err.h"
 #include "lwip/sys.h"
 
 #define EXAMPLE_ESP_WIFI_SSID      "morten_iphone"
-#define EXAMPLE_ESP_WIFI_PASS      "aPwIWF&24ot%"
-#define EXAMPLE_ESP_MAXIMUM_RETRY  4
+#define EXAMPLE_ESP_WIFI_PASS      "aPwIWF&24ot"
+#define EXAMPLE_ESP_MAXIMUM_RETRY  8
 
 #define CONFIG_ESP_WPA3_SAE_PWE_HUNT_AND_PECK true
 #define CONFIG_ESP_WIFI_AUTH_WPA3_PSK         true
@@ -74,6 +87,8 @@ static EventGroupHandle_t s_wifi_event_group;
 static const char* TAG = "wifi device";
 static int s_retry_num = 0;
 
+static bool ping_done = false;
+
 static void wifi_event_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data) {
     if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
         esp_wifi_connect();
@@ -85,13 +100,49 @@ static void wifi_event_handler(void* arg, esp_event_base_t event_base, int32_t e
         } else {
             xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
         }
+        ESP_LOGI(TAG,"connect to the AP fail");
     } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
-        ip_event_got_ip_t* event = (ip_event_got_ip_t*)event_data;
-        ESP_LOGI(TAG, "got ip: " IPSTR, IP2STR(&event->ip_info.ip));
+        ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
+        ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
         s_retry_num = 0;
         xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
     }
 }
+
+
+static void ping_results_callback(esp_ping_handle_t hdl, void* args)
+{
+    printf("Ping Callback\n");
+    int transmitted, received;
+    esp_ping_get_profile(hdl, ESP_PING_PROF_REQUEST, &transmitted, sizeof(transmitted));
+    esp_ping_get_profile(hdl, ESP_PING_PROF_REPLY, &received, sizeof(received));
+
+    printf("Ping: Packets: Sent = %d, Received = %d, Lost = %d (%d%% loss)\n",
+             transmitted, received, transmitted - received,
+             (transmitted - received) * 100 / transmitted);
+
+    ping_done = true;
+}
+
+static void ping_test(void)
+{
+    esp_ping_config_t ping_config = ESP_PING_DEFAULT_CONFIG();
+    ping_config.target_addr = (ip_addr_t){
+        .u_addr.ip4.addr = ipaddr_addr("8.8.8.8")
+    };
+    ping_config.count = 10;  // Number of pings
+
+    esp_ping_callbacks_t cbs = {
+        .on_ping_end = ping_results_callback,
+    };
+
+    esp_ping_handle_t ping;
+    ESP_ERROR_CHECK(esp_ping_new_session(&ping_config, &cbs, &ping));
+    ESP_ERROR_CHECK(esp_ping_start(ping));
+
+    printf("Ping started...\n");
+}
+
 
 void app_main(void)
 {
@@ -111,7 +162,7 @@ void app_main(void)
     // Initialize TCP/IP stack
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
-    esp_netif_create_default_wifi_ap();
+    esp_netif_create_default_wifi_sta();
 
     // Create network interface handle
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
@@ -124,7 +175,7 @@ void app_main(void)
                                                         &wifi_event_handler,
                                                         NULL,
                                                         &instance_any_id));
-    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT,
                                                         IP_EVENT_STA_GOT_IP,
                                                         &wifi_event_handler,
                                                         NULL,
@@ -134,22 +185,23 @@ void app_main(void)
         .sta = {
             .ssid = EXAMPLE_ESP_WIFI_SSID,
             .password = EXAMPLE_ESP_WIFI_PASS,
+            /* Authmode threshold resets to WPA2 as default if password matches WPA2 standards (pasword len => 8).
+             * If you want to connect the device to deprecated WEP/WPA networks, Please set the threshold value
+             * to WIFI_AUTH_WEP/WIFI_AUTH_WPA_PSK and set the password with length and format matching to
+             * WIFI_AUTH_WEP/WIFI_AUTH_WPA_PSK standards.
+             */
             .threshold.authmode = ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD,
             .sae_pwe_h2e = ESP_WIFI_SAE_MODE,
             .sae_h2e_identifier = EXAMPLE_H2E_IDENTIFIER,
-
         },
     };
 
-    if (strlen(EXAMPLE_ESP_WIFI_PASS) == 0) {
-        wifi_config.ap.authmode = WIFI_AUTH_OPEN;
-    }
 
     ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
     ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
     ESP_ERROR_CHECK(esp_wifi_start());
 
-    ESP_LOGI(TAG, "wifi_init_sta finished.");
+    printf("wifi_init_sta finished.\nStarting connection with SSID:%s\n", EXAMPLE_ESP_WIFI_SSID);
 
     // Waiting until either the connection is stablished (WIFI_CONNECTED BIT) or connection failed for the maximum
     // number of tries (WIFI_FAIL_BIT). The bits are set by event_handler() (see above)
@@ -161,10 +213,50 @@ void app_main(void)
 
     // xEventGroupWaitBits() returns the bits before the call returened, hence we can test which event actually happened
     if (bits & WIFI_CONNECTED_BIT) {
-        ESP_LOGI(TAG, "Connected to AP SSID:%s, password:%s", EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS);
+        printf("Connected to AP SSID:%s, password:%s\n", EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS);
     } else if (bits & WIFI_FAIL_BIT) {
-        ESP_LOGI(TAG, "Failed to connect to SSID:%s, password:%s", EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS);
+        printf("Failed to connect to SSID:%s, password:%s\n", EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS);
+        return;
     } else {
-        ESP_LOGI(TAG, "UNEXPECTED EVENT");
+        printf("UNEXPECTED EVENT\n");
+        return;
     }
+
+    printf("Trying to ping google\n");
+    ping_test();
+
+    while (!ping_done) {
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+
+    printf("Trying to connect to NTP server and sync time\n");
+    
+    // Get time before
+    time_t now;
+    time(&now);
+    struct tm timeinfo;
+    localtime_r(&now, &timeinfo);
+    printf("Time before: %s", asctime(&timeinfo));
+    
+    esp_sntp_config_t sntp_config = ESP_NETIF_SNTP_DEFAULT_CONFIG("pool.ntp.org");
+    esp_netif_sntp_init(&sntp_config);
+    
+    if (esp_netif_sntp_sync_wait(pdMS_TO_TICKS(10000)) != ESP_OK) {
+        printf("Failed to update system time within 10s timeout\n");
+    } else {
+        printf("Got good NTP update\n");
+    }
+
+    while (sntp_get_sync_status() == SNTP_SYNC_STATUS_IN_PROGRESS) {
+        vTaskDelay(2000 / portTICK_PERIOD_MS);
+    }
+
+    // Get time after
+    time(&now);
+    localtime_r(&now, &timeinfo);
+    printf("Time after: %s", asctime(&timeinfo));
+
+    // Write time to RTC
+    struct timeval tv = { .tv_sec = now, .tv_usec = 0 };
+    settimeofday(&tv, NULL);
 }
